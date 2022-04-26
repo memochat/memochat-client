484e80877a3dc75618a9eb3482e495553650739d
syoung125 <laura120555@gmail.com>
2022-04-27 02:56:01 +0900

    feat: :lipstick: 디자인 시스템 color 추가

---
+++ src/themes/Color.stories.tsx
@@ -0,0 +1,135 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import styled from '@emotion/styled';
+import { lightTheme } from '.';
+
+export default {
+  title: 'DESIGN SYSTEM/Color',
+  component: () => null,
+  parameters: {
+    viewport: {
+      defaultViewport: 'desktop',
+    },
+  },
+} as ComponentMeta<any>;
+
+const StyledColorWrapper = styled.div`
+  margin: 0.8rem 0.8rem 0.8rem 0;
+  color: ${(p) => p.theme.color.white};
+
+  > *:not(:last-child) {
+    margin-bottom: 0.4rem;
+  }
+
+  > b {
+    display: block;
+    font-weight: bold;
+    color: ${(p) => p.theme.color.white};
+  }
+`;
+
+const StyledColorBlock = styled.div<{ color: string }>`
+  width: 4rem;
+  height: 4rem;
+  border-radius: 0.4rem;
+
+  background-color: ${(p) => p.color};
+`;
+
+const Color = ({ name, color }: { name: string; color: string }) => {
+  return (
+    <StyledColorWrapper>
+      <StyledColorBlock color={color} />
+      <b>{name}</b>
+      <p>{color}</p>
+    </StyledColorWrapper>
+  );
+};
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1.6rem;
+
+  background-color: #262626;
+
+  h1 {
+    margin: 0 0 0.8rem;
+    font-size: 2rem;
+    font-weight: 700;
+    color: ${(p) => p.theme.color.white};
+  }
+
+  h2 {
+    margin: 1.4em 0 1.2rem;
+    font-size: 1.6rem;
+    font-weight: 700;
+    color: ${(p) => p.theme.color.white};
+  }
+
+  h3 {
+    margin: 1em 0 0.4rem;
+    border-bottom: 0.1rem solid ${(p) => p.theme.color.white};
+    padding-bottom: 0.4rem;
+
+    font-weight: bold;
+    color: ${(p) => p.theme.color.white};
+  }
+`;
+
+const StyledRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const ColorList = ({
+  colors,
+  title,
+  namespace,
+  excludeNamespace,
+}: {
+  colors: Record<string, string>;
+  title?: string;
+  namespace?: string;
+  excludeNamespace?: string;
+}) => (
+  <>
+    <h3>{title || namespace}</h3>
+    <StyledRow>
+      {Object.entries(colors)
+        .filter(([key]) => (namespace ? key.includes(namespace) : true))
+        .filter(([key]) => (excludeNamespace ? !key.includes(excludeNamespace) : true))
+        .map(([key, value]) => (
+          <Color key={key} name={key} color={value} />
+        ))}
+    </StyledRow>
+  </>
+);
+
+const Template: ComponentStory<any> = () => {
+  const { color, semanticColor } = lightTheme;
+
+  return (
+    <StyledWrapper>
+      <h1>Design System : Color</h1>
+      <section>
+        <h2>Color</h2>
+        <ColorList colors={color} namespace="white" />
+        <ColorList colors={color} namespace="black" />
+        <ColorList colors={color} namespace="slateGrey" />
+        <ColorList colors={color} namespace="gray" />
+        <ColorList colors={color} namespace="purple" />
+        <ColorList colors={color} namespace="blue" />
+        <ColorList colors={color} namespace="red" />
+        <ColorList colors={color} namespace="green" />
+        <ColorList colors={color} namespace="yellow" />
+      </section>
+      <section>
+        <h2>Semantic Color</h2>
+        <ColorList colors={semanticColor} namespace="background" />
+      </section>
+    </StyledWrapper>
+  );
+};
+
+export const Default = Template.bind({});
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,18 +1,138 @@
-export const lightTheme = {
-  sample: {
-    PRIMARY: '#084B8A',
-    BUTTON_PRIMARY: '#ABCCEC',
-    BUTTON_DEFAULT_FONT: '#435A6F',
-  },
-};
+import { ColorTheme } from './types';
+import { parseColorTheme } from './utils';
 
-export const darkTheme = {
-  sample: {
-    PRIMARY: '#084B8A',
-    BUTTON_PRIMARY: '#565656',
-    BUTTON_DEFAULT_FONT: '#999999',
+export const lightTheme: Readonly<ColorTheme> = parseColorTheme({
+  color: {
+    /** Background Color */
+    white: '#ffffff',
+    black1: '#000000',
+    black2: '#111111',
+    slateGrey: '#EBEEF6',
+
+    gray1: '#333333',
+    gray2: '#4F4F4F',
+    gray3: '#828282',
+    gray4: '#BDBDBD',
+    gray5: '#E0E0E0',
+    gray6: '#F2F2F2',
+    gray7: '#F9F9F9',
+    /** Brand Color */
+    // brand
+    purple1: '#686DF2',
+    purple2: '#888AFB',
+    purple3: '#B7B9FE',
+    purple4: '#B7B9FE',
+    purple5: '#D9DAFF',
+    purple6: '#EBEBFF',
+
+    // primary
+    blue1: '#3D6DF6',
+    blue2: '#6C95F9',
+    blue3: '#8AAEFC',
+    blue4: '#B1CBFE',
+    blue5: '#D8E6FE',
+
+    // secondary
+    orange1: '#F2994A',
+    orange2: '#F7B976',
+    orange3: '#FBCE92',
+    orange4: '#FDE3B7',
+    orange5: '#FEF3DB',
+
+    /** Status Color */
+    // error
+    red1: '#EB5757',
+    red2: '#F38A80',
+    red3: '#F9AB9A',
+    red4: '#FDCDBD',
+    red5: '#FEE9DE',
+
+    // success
+    green1: '#6FCF97',
+    green2: '#93E2AC',
+    green3: '#ADF0BC',
+    green4: '#CBFAD1',
+    green5: '#E5FCE5',
+
+    // warning
+    yellow1: '#F2C94C',
+    yellow2: '#F7DB78',
+    yellow3: '#FBE693',
+    yellow4: '#FDF1B8',
+    yellow5: '#FEF9DB',
   },
-};
+  semanticColor: {
+    background: 'white',
+    backgroundLow: 'slateGrey',
+  },
+});
+
+export const darkTheme: Readonly<ColorTheme> = parseColorTheme({
+  color: {
+    /** Background Color */
+    white: '#ffffff',
+    black1: '#000000',
+    black2: '#111111',
+    slateGrey: '#EBEEF6',
+
+    gray1: '#333333',
+    gray2: '#4F4F4F',
+    gray3: '#828282',
+    gray4: '#BDBDBD',
+    gray5: '#E0E0E0',
+    gray6: '#F2F2F2',
+    gray7: '#F9F9F9',
+
+    /** Brand Color */
+    // brand
+    purple1: '#686DF2',
+    purple2: '#888AFB',
+    purple3: '#B7B9FE',
+    purple4: '#B7B9FE',
+    purple5: '#D9DAFF',
+    purple6: '#EBEBFF',
+
+    // primary
+    blue1: '#3D6DF6',
+    blue2: '#6C95F9',
+    blue3: '#8AAEFC',
+    blue4: '#B1CBFE',
+    blue5: '#D8E6FE',
+
+    // secondary
+    orange1: '#F2994A',
+    orange2: '#F7B976',
+    orange3: '#FBCE92',
+    orange4: '#FDE3B7',
+    orange5: '#FEF3DB',
+
+    /** Status Color */
+    // error
+    red1: '#EB5757',
+    red2: '#F38A80',
+    red3: '#F9AB9A',
+    red4: '#FDCDBD',
+    red5: '#FEE9DE',
+
+    // success
+    green1: '#6FCF97',
+    green2: '#93E2AC',
+    green3: '#ADF0BC',
+    green4: '#CBFAD1',
+    green5: '#E5FCE5',
+
+    // warning
+    yellow1: '#F2C94C',
+    yellow2: '#F7DB78',
+    yellow3: '#FBE693',
+    yellow4: '#FDF1B8',
+    yellow5: '#FEF9DB',
+  },
+  semanticColor: {
+    background: 'white',
+    backgroundLow: 'slateGrey',
+  },
+});
 
 export type EmotionTheme = typeof lightTheme;
 
