6b98b0bc56fa19e659b5539afdf5b517c1cdeeaf
syoung125 <laura120555@gmail.com>
2022-07-19 01:34:00 +0900

    ✨ feat: 아이콘 컴포넌트 추가

---
+++ src/components/reusable/Icon/Icon.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Icon from './index';
+
+export default {
+  title: 'Components/reusable/Icon',
+  argTypes: {
+    name: { control: 'text' },
+    color: { control: 'text' },
+    size: { control: 'number', name: 'size(px)' },
+    width: { control: 'text', name: 'width(px)' },
+    height: { control: 'text', name: 'height(px)' },
+  },
+  args: {},
+} as ComponentMeta<typeof Icon>;
+
+const Template: ComponentStory<typeof Icon> = ({ ...args }) => <Icon {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  name: 'Album',
+  color: 'black1',
+  size: 30,
+};
