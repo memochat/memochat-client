36e05c981a9476b07b484934989e1b1b39b71365
syoung125 <laura120555@gmail.com>
2022-07-27 15:58:51 +0900

    ✨ feat: textField 컴포넌트 개선

---
+++ src/components/reusable/TextField/TextField.stories.tsx
@@ -0,0 +1,72 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { useState } from 'react';
+
+import TextField from './index';
+
+export default {
+  title: 'Components/reusable/TextField',
+  component: TextField,
+  args: {
+    id: 'test',
+    label: '아이디',
+    placeholder: '아이디를 입력하세요',
+    value: '',
+  },
+  argTypes: {
+    label: {
+      control: { type: 'text' },
+    },
+    placeholder: {
+      control: { type: 'text' },
+    },
+    type: {
+      control: { type: 'radio' },
+      options: ['text', 'password'],
+    },
+    maxLength: {
+      control: { type: 'number' },
+    },
+    error: {
+      control: { type: 'boolean' },
+    },
+    errorMessage: {
+      control: { type: 'text' },
+    },
+    success: {
+      control: { type: 'boolean' },
+    },
+    successMessage: {
+      control: { type: 'text' },
+    },
+    helperMessage: {
+      control: { type: 'text' },
+    },
+  },
+} as ComponentMeta<typeof TextField>;
+
+const Default: ComponentStory<typeof TextField> = (args) => {
+  const [value, setValue] = useState('');
+
+  return <TextField {...args} value={value} onChange={setValue} />;
+};
+
+export const 기본 = Default.bind({});
+기본.args = {};
+
+export const 최대_글자수_제한 = Default.bind({});
+최대_글자수_제한.args = {
+  helperMessage: '아이디는 10글자 이내 입니다',
+  maxLength: 10,
+};
+
+export const 에러 = Default.bind({});
+에러.args = {
+  error: true,
+  errorMessage: '에러메세지',
+};
+
+export const 성공 = Default.bind({});
+성공.args = {
+  success: true,
+  successMessage: '성공메세지',
+};
---
+++ src/components/reusable/TextField/styled.ts
@@ -0,0 +1,109 @@
+import styled from '@emotion/styled';
+
+/** @todo 지우기 */
+// eslint-disable-next-line import/no-cycle
+import { TextFieldProps } from '.';
+
+import { EmotionTheme } from '@src/themes';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const getLabelColor = ({
+  error,
+  theme,
+}: Pick<TextFieldProps, 'error'> & {
+  theme: EmotionTheme;
+}) => {
+  return error ? theme.color.red1 : theme.color.purple1;
+};
+
+export const Label = styled.label<Pick<TextFieldProps, 'error'>>`
+  color: ${(p) => getLabelColor(p)};
+  ${(p) => p.theme.typography.body3};
+`;
+
+const getInputBorderBottomColor = ({
+  error,
+  success,
+  isFocused,
+  theme,
+}: Pick<TextFieldProps, 'error' | 'success'> & { isFocused: boolean; theme: EmotionTheme }) => {
+  if (error) {
+    return theme.color.red1;
+  }
+
+  if (success || isFocused) {
+    return theme.color.purple1;
+  }
+
+  return theme.color.gray3;
+};
+
+export const InputWrapper = styled.div<
+  Pick<TextFieldProps, 'error' | 'success'> & { isFocused: boolean }
+>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 12px 8px;
+  border-bottom: 1px solid ${(p) => getInputBorderBottomColor(p)};
+`;
+
+export const Input = styled.input`
+  width: 100%;
+
+  color: ${(p) => p.theme.color.gray1};
+  ${(p) => p.theme.typography.h4};
+
+  &::placeholder,
+  &::-webkit-input-placeholder,
+  &:-ms-input-placeholder {
+    color: ${(p) => p.theme.color.gray5};
+  }
+
+  /* Chrome autofill 스타일 제거, 커스텀 */
+  &:-webkit-autofill,
+  &:-webkit-autofill:hover,
+  &:-webkit-autofill:focus,
+  &:-webkit-autofill:active {
+    transition: background-color 5000s ease-in-out 0s;
+    -webkit-transition: background-color 9999s ease-out;
+    -webkit-box-shadow: 0 0 0px 1000px transparent inset;
+    -webkit-text-fill-color: ${(p) => p.theme.color.gray1};
+  }
+`;
+
+const getHelperMessageColor = ({
+  error,
+  success,
+  theme,
+}: Pick<TextFieldProps, 'error' | 'success'> & {
+  theme: EmotionTheme;
+}) => {
+  if (error) {
+    return theme.color.red1;
+  }
+
+  if (success) {
+    return theme.color.purple1;
+  }
+
+  return theme.color.gray3;
+};
+
+export const MaxLength = styled.span`
+  margin-left: 8px;
+
+  color: ${(p) => p.theme.color.gray2};
+  ${(p) => p.theme.typography.body4};
+`;
+
+export const HelperMessage = styled.p<Pick<TextFieldProps, 'error' | 'success'>>`
+  margin-top: 3px;
+
+  color: ${(p) => getHelperMessageColor(p)};
+  ${(p) => p.theme.typography.body4};
+`;
