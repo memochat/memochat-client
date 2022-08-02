3ba2ae45c73fe851ed584959cfbc7fd580628721
syoung125 <laura120555@gmail.com>
2022-08-03 01:05:27 +0900

    ✨ feat: UpsertRoomDialog 컴포넌트 추가

---
+++ src/components/reusable/room/UpsertRoomDialog/UpsertRoomDialog.styles.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  padding: 16px;
+
+  > *:not(:last-child) {
+    margin-bottom: 24px;
+  }
+`;
---
+++ src/components/reusable/room/UpsertRoomDialog/index.ts
@@ -0,0 +1,2 @@
+export { default } from './UpsertRoomDialog';
+export type { UpsertRoomDialogProps } from './UpsertRoomDialog.types';
