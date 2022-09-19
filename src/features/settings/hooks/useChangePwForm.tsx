8ba770c84cd28225c5a0db8858c5dbc47016d6e3
sooohka <shkorea1004@gmail.com>
2022-09-19 21:52:36 +0900

    feat: :sparkles: add 비밀번호 변경 페이지

---
+++ src/features/settings/hooks/useChangePwForm.tsx
@@ -0,0 +1,45 @@
+import { useForm, UseFormProps } from 'react-hook-form';
+import z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+export interface ChangePwFormType {
+  currentPassword: string;
+  password: string;
+  password2: string;
+}
+
+const defaultValues: ChangePwFormType = {
+  currentPassword: '',
+  password: '',
+  password2: '',
+};
+
+const schema = z
+  .object({
+    currentPassword: z.string({ required_error: '필수값입니다.' }),
+    password: z
+      .string({ required_error: '필수값입니다.' })
+      .min(3, '최소 3자 이상 입력해주세요.')
+      .max(20, { message: '최대 20자까지 가능합니다.' }),
+    password2: z.string({ required_error: '필수값입니다.' }).max(20),
+  })
+  .refine(
+    (v) => {
+      return v.password === v.password2;
+    },
+    {
+      path: ['password2'],
+      message: '비밀번호가 일치하지 않습니다.',
+    },
+  );
+
+const useChangePwForm = (props?: UseFormProps<ChangePwFormType>) => {
+  return useForm<ChangePwFormType>({
+    ...props,
+    defaultValues: props?.defaultValues || defaultValues,
+    mode: 'all',
+    resolver: zodResolver(schema),
+  });
+};
+
+export default useChangePwForm;
---
+++ src/pages/settings/account/change-pw/index.page.tsx
@@ -0,0 +1,123 @@
+import { NextPage } from 'next';
+import { useState } from 'react';
+import { Controller, SubmitHandler } from 'react-hook-form';
+
+import * as S from './changepw.styles';
+
+import useChangePwForm, { ChangePwFormType } from '@src/features/settings/hooks/useChangePwForm';
+import { Button, Stepper, TextField } from '@src/shared/components';
+import useConfirm from '@src/shared/hooks/useConfirm';
+
+const SignUp: NextPage = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const { control, handleSubmit, getFieldState, formState } = useChangePwForm();
+  const { confirm } = useConfirm();
+
+  const handleIdNextBtnClick = () => {
+    const { isDirty, error } = getFieldState('currentPassword');
+
+    //TODO:패스워드 검증 필요
+
+    if (isDirty && error) {
+      //TODO: 나중에 에러 Alert수정
+      confirm({
+        headerTitle: '에러',
+        title: '문제가 발생했습니다!',
+        description: error.message || '',
+      });
+      return;
+    }
+    setActiveIndex((prev) => prev + 1);
+  };
+
+  const onSubmit: SubmitHandler<ChangePwFormType> = (values) => {
+    alert(JSON.stringify(values, null, 2));
+  };
+
+  return (
+    <S.Wrapper as="form" onSubmit={handleSubmit(onSubmit)}>
+      <S.Title>
+        비밀번호 변경을 위해
+        <br /> 현재 비밀번호를 입력해주세요.
+      </S.Title>
+      <Stepper
+        activeIndex={activeIndex}
+        css={{
+          '& .stepper-connector': {
+            marginBottom: '16px',
+          },
+        }}
+      >
+        <S.Content>
+          <div style={{ flexGrow: 1 }}>
+            <Controller
+              control={control}
+              name="currentPassword"
+              render={({ field, fieldState }) => (
+                <TextField
+                  id={`currentPassword-textfield`}
+                  value={field.value}
+                  onChange={field.onChange}
+                  onBlur={field.onBlur}
+                  label="현재 비밀번호(최대 20자)"
+                  error={Boolean(fieldState.error)}
+                  errorMessage={fieldState.error?.message}
+                  success={fieldState.isDirty && !fieldState.error}
+                  successMessage="비밀번호가 일치합니다."
+                  maxLength={20}
+                />
+              )}
+            />
+          </div>
+          <Button onClick={handleIdNextBtnClick}>계속</Button>
+        </S.Content>
+        <S.Content>
+          <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column', gap: '30px' }}>
+            <Controller
+              control={control}
+              name="password"
+              render={({ field, fieldState }) => (
+                <TextField
+                  type="password"
+                  id={`password-textfield`}
+                  value={field.value}
+                  onChange={field.onChange}
+                  label="변경할 비밀번호"
+                  error={Boolean(fieldState.error)}
+                  errorMessage={fieldState.error?.message}
+                  success={fieldState.isDirty && !fieldState.error}
+                  successMessage="사용가능한 비밀번호입니다."
+                  helperMessage="최대 20자까지 입력 가능합니다."
+                  maxLength={20}
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="password2"
+              render={({ field, fieldState }) => (
+                <TextField
+                  type="password"
+                  id={`password2-textfield`}
+                  value={field.value}
+                  onChange={field.onChange}
+                  label="변경할 비밀번호 확인"
+                  error={Boolean(fieldState.error)}
+                  errorMessage={fieldState.error?.message}
+                  success={fieldState.isDirty && !fieldState.error}
+                  successMessage="비밀번호가 일치합니다."
+                  maxLength={20}
+                />
+              )}
+            />
+          </div>
+          <Button disabled={!formState.isValid} type="submit">
+            변경
+          </Button>
+        </S.Content>
+      </Stepper>
+    </S.Wrapper>
+  );
+};
+
+export default SignUp;
