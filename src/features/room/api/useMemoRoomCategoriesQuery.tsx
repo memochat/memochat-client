1a2fd960a78619eee6fcc9f77ee461868d401dee
syoung125 <laura120555@gmail.com>
2022-10-27 13:15:31 +0900

    feat: :sparkles: 룸 카테고리 staleTime, cacheTime 수정

--- src/features/room/api/useMemoRoomCategoriesQuery.tsx
+++ src/features/room/api/useMemoRoomCategoriesQuery.tsx
@@ -8,12 +8,15 @@ import { memoRoomCategoryKeys } from '@src/shared/utils/queryKeys';
 const getMemoRoomCategories = () =>
   axios.get<GetMemoRoomCategories['res']>('/memo-rooms/categories');
 
+const ONE_HOUR = 60 * 60 * 1000;
+
+// TODO: 결과값 로컬 스토리지에 저장
 const useMemoRoomCategoriesQuery = (
   options?: UseQueryOptions<AxiosResponse<GetMemoRoomCategories['res']>>,
 ) =>
   useQuery(memoRoomCategoryKeys.list(), getMemoRoomCategories, {
-    // 1주일 캐싱
-    cacheTime: 7 * 24 * 60 * 60 * 1000,
+    staleTime: ONE_HOUR,
+    cacheTime: ONE_HOUR,
     ...options,
   });
 
