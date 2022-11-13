cb24d2579e5308abfd0f20bc382cf2307d79bd5f
Ko Seoyoung <laura120555@gmail.com>
2022-11-13 12:43:15 +0900

    feat: ✨ 룸 설정 페이지 api 연동 (#46)
    * refactor: :recycle: 룸 설정 페이지 > 공통 Header 사용하도록 리팩토링
    
    * feat: :sparkles: 룸 설정 페이지 api 연동
    
    * refactor: :recycle:

--- src/features/room/api/useDeleteMemoRoomMutation.tsx
+++ src/features/room/api/useDeleteMemoRoomMutation.tsx
@@ -2,10 +2,8 @@ import { useMutation, UseMutationOptions } from '@tanstack/react-query';
 
 import axios from '@src/shared/configs/axios';
 import { DeleteMemoRooms } from '@src/shared/types/api/memoRooms';
-import { queryClient } from '@src/shared/configs/react-query';
-import { memoRoomKeys } from '@src/shared/utils/queryKeys';
 
-export const deleteMemoRoom = async (id: number) => {
+export const deleteMemoRoom = async ({ id }: { id: number }) => {
   const res = await axios.delete<DeleteMemoRooms['res']>(`/memo-rooms/${id}`);
   return res.data;
 };
@@ -13,11 +11,6 @@ export const deleteMemoRoom = async (id: number) => {
 const useDeleteMemoRoomMutation = (
   options?: UseMutationOptions<DeleteMemoRooms['res'], unknown, { id: number }>,
 ) => {
-  return useMutation(({ id }: { id: number }) => deleteMemoRoom(id), {
-    onSuccess: () => {
-      queryClient.invalidateQueries(memoRoomKeys.list());
-    },
-    ...options,
-  });
+  return useMutation(deleteMemoRoom, options);
 };
 export default useDeleteMemoRoomMutation;
---
+++ src/features/room/api/useMemoRoomQuery.tsx
@@ -0,0 +1,15 @@
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+
+import axios from '@src/shared/configs/axios';
+import { GetMemoRoom } from '@src/shared/types/api/memoRooms';
+import { memoRoomKeys } from '@src/shared/utils/queryKeys';
+
+export const getMemoRoom = async (id: number) => {
+  const res = await axios.get<GetMemoRoom['res']>(`/memo-rooms/${id}`);
+  return res.data;
+};
+
+const useMemoRoomQuery = (id: number, options?: UseQueryOptions<GetMemoRoom['res']>) =>
+  useQuery(memoRoomKeys.detail(id), () => getMemoRoom(id), options);
+
+export default useMemoRoomQuery;
