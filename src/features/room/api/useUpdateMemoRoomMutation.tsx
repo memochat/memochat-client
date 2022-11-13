f8b819d17169863a54f92cbe2b3584d0725bcce4
syoung125 <laura120555@gmail.com>
2022-11-13 13:31:46 +0900

    refactor: :recycle: updateMemoRoom prop 구조 수정

--- src/features/room/api/useUpdateMemoRoomMutation.tsx
+++ src/features/room/api/useUpdateMemoRoomMutation.tsx
@@ -3,7 +3,13 @@ import { useMutation, UseMutationOptions } from '@tanstack/react-query';
 import axios from '@src/shared/configs/axios';
 import { UpdateMemoRooms } from '@src/shared/types/api/memoRooms';
 
-export const updateMemoRoom = async (id: number, param: UpdateMemoRooms['param']) => {
+export const updateMemoRoom = async ({
+  id,
+  param,
+}: {
+  id: number;
+  param: UpdateMemoRooms['param'];
+}) => {
   const res = await axios.put<UpdateMemoRooms['res']>(`/memo-rooms/${id}`, param);
   return res.data;
 };
@@ -14,10 +20,6 @@ const useUpdateMemoRoomMutation = (
     unknown,
     { id: number; param: UpdateMemoRooms['param'] }
   >,
-) =>
-  useMutation(
-    ({ id, param }: { id: number; param: UpdateMemoRooms['param'] }) => updateMemoRoom(id, param),
-    options,
-  );
+) => useMutation(updateMemoRoom, options);
 
 export default useUpdateMemoRoomMutation;
