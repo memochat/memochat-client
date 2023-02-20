a0f468f86d3d9c56cfb5e70be54838d380c9276b
sooohka <sooohka01@gmail.com>
2023-02-20 22:09:51 +0900

    feat: :sparkles: add env

--- global.d.ts
+++ global.d.ts
@@ -7,6 +7,13 @@ declare global {
     };
     MemochatWebview: NativeBridge;
   }
+
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production';
+      NEXT_PUBLIC_API_URL;
+    }
+  }
 }
 
 export {};
