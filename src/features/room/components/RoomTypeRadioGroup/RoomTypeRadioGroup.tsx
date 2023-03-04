8e114e76b03c4ae0129ffb72f1412102294a0354
sooohka <sooohka01@gmail.com>
2023-03-04 23:25:18 +0900

    fix: :bug: RoomTypeRadioGroup, forwardRef 에러 수정

--- src/features/room/components/RoomTypeRadioGroup/RoomTypeRadioGroup.tsx
+++ src/features/room/components/RoomTypeRadioGroup/RoomTypeRadioGroup.tsx
@@ -1,55 +1,54 @@
-import { useEffect, useState } from 'react';
+import { forwardRef, useEffect, useState } from 'react';
 
-import { RoomTypeRadioGroupProps } from './RoomTypeRadioGroup.types';
-import * as S from './RoomTypeRadioGroup.styles';
 import useMemoRoomCategoriesQuery from '../../api/useMemoRoomCategoriesQuery';
+import * as S from './RoomTypeRadioGroup.styles';
+import { RoomTypeRadioGroupProps } from './RoomTypeRadioGroup.types';
 
-const RoomTypeRadioGroup = ({
-  name,
-  label,
-  value,
-  onChange,
-  className,
-}: RoomTypeRadioGroupProps) => {
-  const [selectedRoomTypeId, setSelectedRoomTypeId] = useState(value);
+const RoomTypeRadioGroup = forwardRef<HTMLInputElement, RoomTypeRadioGroupProps>(
+  ({ name, label, value, onChange, className }, ref) => {
+    const [selectedRoomTypeId, setSelectedRoomTypeId] = useState(value);
 
-  const { data: memoRoomCategories, isLoading } = useMemoRoomCategoriesQuery();
+    const { data: memoRoomCategories, isLoading } = useMemoRoomCategoriesQuery();
 
-  useEffect(() => {
-    setSelectedRoomTypeId(value);
-  }, [value]);
+    useEffect(() => {
+      setSelectedRoomTypeId(value);
+    }, [value]);
 
-  const handleRoomTypeClick = (id: number) => {
-    setSelectedRoomTypeId(id);
-    onChange?.(id);
-  };
+    const handleRoomTypeClick = (id: number) => {
+      setSelectedRoomTypeId(id);
+      onChange?.(id);
+    };
 
-  if (isLoading) {
-    // TODO : 스켈레톤
-    return null;
-  }
+    if (isLoading) {
+      // TODO : 스켈레톤
+      return null;
+    }
 
-  return (
-    <S.Wrapper className={className}>
-      <S.Label>{label}</S.Label>
-      <S.RoomTypeList>
-        {memoRoomCategories?.map((roomType) => (
-          <label key={roomType.id}>
-            <input
-              type="radio"
-              name={name}
-              value={roomType.id}
-              checked={roomType.id === selectedRoomTypeId}
-              onChange={(e) => handleRoomTypeClick(parseInt(e.target.value))}
-            />
-            <S.RoomType isSelected={roomType.id === selectedRoomTypeId}>
-              <img src={roomType.thumbnail} alt={roomType.name} />
-            </S.RoomType>
-          </label>
-        ))}
-      </S.RoomTypeList>
-    </S.Wrapper>
-  );
-};
+    return (
+      <S.Wrapper className={className}>
+        <S.Label>{label}</S.Label>
+        <S.RoomTypeList>
+          {memoRoomCategories?.map((roomType) => (
+            <label key={roomType.id}>
+              <input
+                type="radio"
+                name={name}
+                ref={ref}
+                value={roomType.id}
+                checked={roomType.id === selectedRoomTypeId}
+                onChange={(e) => handleRoomTypeClick(parseInt(e.target.value))}
+              />
+              <S.RoomType isSelected={roomType.id === selectedRoomTypeId}>
+                <img src={roomType.thumbnail} alt={roomType.name} />
+              </S.RoomType>
+            </label>
+          ))}
+        </S.RoomTypeList>
+      </S.Wrapper>
+    );
+  },
+);
+
+RoomTypeRadioGroup.displayName = 'RoomTypeRadioGroup';
 
 export default RoomTypeRadioGroup;
