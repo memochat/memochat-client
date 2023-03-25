23b0a5fd4db88006146c63079783298aea0b1d17
syoung125 <laura120555@gmail.com>
2023-03-26 01:49:59 +0900

    Revert "chore: 사용안하는 logo.svg 삭제"
    This reverts commit eb20b0603a6ae9c5381d6ce0ccd0b17c1cf0183e.

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -9,6 +9,7 @@ export { ReactComponent as Check } from './check.svg';
 export { ReactComponent as Close } from './close.svg';
 export { ReactComponent as Download } from './download.svg';
 export { ReactComponent as Delete } from './ic-delete.svg';
+export { ReactComponent as Logo } from './logo.svg';
 export { ReactComponent as Pin } from './pin.svg';
 export { ReactComponent as Plus } from './plus.svg';
 export { ReactComponent as Reply } from './reply.svg';
