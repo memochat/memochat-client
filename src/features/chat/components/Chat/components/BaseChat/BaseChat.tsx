2d9e769f57c4953334fb9d1e16b2d3e0a838d076
sooohka <sooohka01@gmail.com>
2023-05-19 23:16:40 +0900

    feat: :sparkles: 길게 누르는 액션 커스텀훅으로 분리 useLongClick

--- src/features/chat/components/Chat/components/BaseChat/BaseChat.tsx
+++ src/features/chat/components/Chat/components/BaseChat/BaseChat.tsx
@@ -1,32 +1,21 @@
 import dayjs from 'dayjs';
-import { TouchEvent, useEffect, useRef } from 'react';
+
+import useLongClick from '@src/shared/hooks/useLongClick';
 
 import * as S from './BaseChat.styles';
 import { BaseChatProps } from './BaseChat.types';
 
 const BaseChat = ({ message, createdAt, onOpenContextMenu }: BaseChatProps) => {
-  const ref = useRef(null);
-  const handleContextMenu = async (e: TouchEvent<HTMLDivElement>) => {
+  const { onTouchEnd, onTouchStart } = useLongClick<HTMLDivElement>((e) => {
     const { clientX, clientY } = e.touches[0];
-    ref.current = setTimeout(() => {
-      onOpenContextMenu({ x: clientX, y: clientY });
-    }, 1000);
-  };
-
-  useEffect(() => {
-    return () => {
-      ref.current = null;
-      clearTimeout(ref.current);
-    };
-  }, []);
+    onOpenContextMenu({ x: clientX, y: clientY });
+  });
 
   return (
     <S.Wrapper
       onContextMenu={(e) => e.preventDefault()}
-      onTouchStart={handleContextMenu}
-      onTouchEnd={() => {
-        clearTimeout(ref.current);
-      }}
+      onTouchStart={onTouchStart}
+      onTouchEnd={onTouchEnd}
     >
       <S.Message>{message}</S.Message>
       <S.Date>{`${dayjs(createdAt).format('hh:mm A')}`}</S.Date>
